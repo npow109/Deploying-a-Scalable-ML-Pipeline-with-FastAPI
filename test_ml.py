0b4e3b25f1b9a93193f80bbaa20a356c9f05841d
npow109 <npow109@wgu.edu>
2025-12-15 12:19:49 -0500

    unit_test.png

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,46 @@
 import pytest
 # TODO: add necessary import
+from ml.model import compute_model_metrics, train_model, inference
+from numpy import random
+from sklearn.ensemble import RandomForestClassifier
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_compute_metrics():
     """
-    # add description for the first test
+    # Checks that the compute_metrics function correctly returns the 
+    # precision, recall, and fbeta variables 
     """
     # Your code here
-    pass
+    y = [1,0,1,0,0,0,0,1,0,1,0]
+    preds = [1,0,1,0,0,0,0,1,0,1,1]
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert(precision is not None)
+    assert(recall is not None)
+    assert(fbeta is not None)
+
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_train_model():
     """
-    # add description for the second test
+    # Checks that the train_model function returns the correct model 
+    # (RandomForestClassifier)
     """
     # Your code here
-    pass
+    X_train = random.randint(10, size=(3, 20))
+    y_train = random.randint(1, size=(3))
+    i = train_model(X_train, y_train)
+    assert(isinstance(i, RandomForestClassifier))
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_inference():
     """
-    # add description for the third test
+    # Checks that the inference function returns the preds variable
     """
     # Your code here
-    pass
+    X_train = random.randint(10, size=(3, 20))
+    y_train = random.randint(1, size=(3))
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_train)
+    assert(preds is not None)
